591de0ee88a4024e122f2ed1de492f09a6424dea
Abdur Rehman <muhammadbilalakbar021@gmail.com>
2022-03-18 00:18:42 +0500

    Update ip-provider.ts

--- src/app/utils/ip-provider.ts
+++ src/app/utils/ip-provider.ts
@@ -4,8 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class IpProvider {
-  // ip: string = 'https://api.oaims-tex.com/api';
-  ip: string = 'http://192.168.166.57:8080/api';
+  ip: string = 'https://api.oaims-tex.com/api';
   constructor() {}
 
   getHostIp() {
